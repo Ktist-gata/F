e4890beed0b454981752509796778e6f809c269b
Kata <rkgrbg@gmail.com>
2025-04-02 14:30:36 +0800

    Update data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const infoList = [
+  { id: 1, title: "HTML 是什麼", content: "HTML 是網頁的骨架。" },
+  { id: 2, title: "CSS 的功能", content: "CSS 控制網頁的外觀與排版。" },
+  { id: 3, title: "JavaScript 基礎", content: "讓網頁可以互動和變化。" }
+];
